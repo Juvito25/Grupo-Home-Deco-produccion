bbb07712321fe619eec902f792e701cd26550d72
Juvito25 <juviwebsolution@gmail.com>
2025-08-27 10:25:03 -0300

    fix: Solución sobre el link de 'Cerrar sesión' en el panel de administración

--- template-admin-dashboard.php
+++ template-admin-dashboard.php
@@ -19,19 +19,7 @@ get_header(); // Carga el header de WordPress
     
     <!-- BARRA LATERAL (ADMINISTRADOR) -->
     <aside class="ghd-sidebar">
-        <div class="sidebar-header">
-            <h1 class="logo">Gestor de Producción</h1>
-        </div>
-        <nav class="sidebar-nav">
-            <ul>
-                <li class="active"><a href="#"><i class="fa-solid fa-table-columns"></i> <span>Panel de Control</span></a></li>
-                <li><a href="#"><i class="fa-solid fa-plus"></i> <span>Nuevo Pedido</span></a></li>
-                <li><a href="#"><i class="fa-solid fa-cubes"></i> <span>Sectores de Producción</span></a></li>
-                <li><a href="#"><i class="fa-solid fa-users"></i> <span>Clientes</span></a></li>
-                <li><a href="#"><i class="fa-solid fa-chart-pie"></i> <span>Reportes</span></a></li>
-                <li><a href="#"><i class="fa-solid fa-gear"></i> <span>Configuración</span></a></li>
-            </ul>
-        </nav>
+       <?php get_template_part('template-parts/sidebar-admin'); ?> 
     </aside>
 
     <!-- CONTENIDO PRINCIPAL -->
